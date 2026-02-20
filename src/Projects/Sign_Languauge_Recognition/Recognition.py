609a5fe7ea0c2b3ab830658f940d567e1e57ca81
HannahSMMcCormick <114184694+HannahSMMcCormick@users.noreply.github.com>
2026-02-20 12:43:47 +0000

    pose landmarks

--- src/Projects/Sign_Languauge_Recognition/Recognition.py
+++ src/Projects/Sign_Languauge_Recognition/Recognition.py
@@ -7,7 +7,7 @@ import numpy as np
 import matplotlib as plt
 import cv2 
 import os
-from config import EXTERNAL_PATH, INTERIM_PATH, hand_landmarker_task_PATH, Face_Landmarker_task_PATH
+from config import EXTERNAL_PATH, INTERIM_PATH, hand_landmarker_task_PATH, Face_Landmarker_task_PATH, pose_landmarker_task_PATH
 
 #MediaPipe Classes
 mp_hands = mp.tasks.vision.HandLandmarksConnections #Joints and fingers
@@ -21,7 +21,7 @@ HANDEDNESS_TEXT_COLOR = (88, 205, 54)  #Colour
 
 
 #Takes image and annotates it 
-def draw_landmarks_on_image(rgb_image, hand_detection_result, face_detection_result):
+def draw_landmarks_on_image(rgb_image, hand_detection_result, face_detection_result, pose_detection_result):
   
   #Hand Landmarks
   hand_landmarks_list = hand_detection_result.hand_landmarks #List of hands
@@ -31,7 +31,12 @@ def draw_landmarks_on_image(rgb_image, hand_detection_result, face_detection_res
   # Face Landmarks
   face_landmarks_list = face_detection_result.face_landmarks 
   
-
+  #Pose Landmarks
+  pose_landmarks_list = pose_detection_result.pose_landmarks #check dependencies
+  pose_landmark_style = drawing_styles.get_default_pose_landmarks_style()
+  pose_connection_style = drawing_utils.DrawingSpec(color=(0, 255, 0), thickness=2)
+  
+  
   # Loop through the detected hands to visualize.
   for idx in range(len(hand_landmarks_list)):
     hand_landmarks = hand_landmarks_list[idx] #Joint landmarks
@@ -87,10 +92,15 @@ def draw_landmarks_on_image(rgb_image, hand_detection_result, face_detection_res
         connections=vision.FaceLandmarksConnections.FACE_LANDMARKS_RIGHT_IRIS,
           landmark_drawing_spec=None,
           connection_drawing_spec=drawing_styles.get_default_face_mesh_iris_connections_style())
-    
-    
   
-  
+  for pose_landmarks in pose_landmarks_list:
+    drawing_utils.draw_landmarks(
+        image=annotated_image,
+        landmark_list=pose_landmarks,
+        connections=vision.PoseLandmarksConnections.POSE_LANDMARKS,
+        landmark_drawing_spec=pose_landmark_style,
+        connection_drawing_spec=pose_connection_style)
+
   return annotated_image
 
 
@@ -116,6 +126,15 @@ def FaceObject():
   
   return detector
   
+def poseObject():
+  
+  base_options = python.BaseOptions(model_asset_path= str(pose_landmarker_task_PATH))
+  options = vision.PoseLandmarkerOptions(
+    base_options=base_options,
+    output_segmentation_masks=True)
+  detector = vision.PoseLandmarker.create_from_options(options)
+  
+  return detector
 
 def get_video():
   
@@ -153,6 +172,8 @@ def get_video():
     
     face_landmark_detector = FaceObject()
     
+    pose_landmark_detector = poseObject()
+    
     
     while True:
       
@@ -168,12 +189,12 @@ def get_video():
       
       hand_detection_result = hand_landmark_detector.detect(mp_image) #Detects hand landmarks
       face_detection_result = face_landmark_detector.detect(mp_image) #Detects face landmarks
+      pose_detection_result = pose_landmark_detector.detect(mp_image)
       
       
       
       
-      
-      annotated_rgb = draw_landmarks_on_image(frame_rgb, hand_detection_result, face_detection_result) #Use the function to draw the landmarks on
+      annotated_rgb = draw_landmarks_on_image(frame_rgb, hand_detection_result, face_detection_result, pose_detection_result) #Use the function to draw the landmarks on
       
       
       annotated_bgr = cv2.cvtColor(annotated_rgb, cv2.COLOR_RGB2BGR) #convert back to bgr
